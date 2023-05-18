e78eb17efd788ac2bee142c87f35e450a54fd242
Makesi Morrison <makmorrison@gmail.com>
2023-05-18 18:32:45 -0500

    Feat: added the auth:sanctum middleware to all endpoints in the book controller except index and show

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,6 +10,11 @@ use Illuminate\Database\Eloquent\Builder;
 
 class BookController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware("auth:sanctum")->except(["index", "show"]);
+    }
+
     /**
      * Display a listing of the resource.
      */
