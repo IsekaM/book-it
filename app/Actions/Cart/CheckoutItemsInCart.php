46a0de2caed6aed8895ca3743698099c35718dfc
Makesi Morrison <makmorrison@gmail.com>
2023-05-18 14:48:31 -0500

    Feat: added GetPaymentDetails action to CheckoutItemsInCart action

--- app/Actions/Cart/CheckoutItemsInCart.php
+++ app/Actions/Cart/CheckoutItemsInCart.php
@@ -3,18 +3,21 @@
 namespace App\Actions\Cart;
 
 use App\Models\Cart;
+use App\DataTransferObjects\WiPay\WiPayResponse;
 
 class CheckoutItemsInCart
 {
     public function __construct(
         private readonly CheckIfItemsInCart $checkIfItemsInCart,
         private readonly CheckIfOrderIsProcessed $checkIfOrderIsProcessed,
+        private readonly GetPaymentDetails $getPaymentDetails,
     ) {
     }
 
-    public function execute(Cart $cart): void
+    public function execute(Cart $cart): WiPayResponse
     {
         $this->checkIfItemsInCart->execute($cart);
         $this->checkIfOrderIsProcessed->execute($cart);
+        return $this->getPaymentDetails->execute($cart);
     }
 }
